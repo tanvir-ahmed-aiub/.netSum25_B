0330036b0136756f7e0e63e8638590374471b100
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-07-21 14:22:49 +0600

    Form Submission/Validation

---
+++ FormSubmission/FormSubmission/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.Models
+{
+    public class Student
+    {
+        [Required]
+        [Range(1,40)]
+        public int? Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is needed")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(500,MinimumLength =20)]
+        public string Address { get; set; }
+        
+    }
+}
