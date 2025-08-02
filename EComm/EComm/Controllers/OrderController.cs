64d7dda77c044c1999009dcb6442483654f75ab0
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-08-02 15:43:12 +0600

    Update

---
+++ EComm/EComm/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using EComm.DTOs;
+using EComm.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EComm.Controllers
+{
+    public class OrderController : Controller
+    {
+        EComm_Sum25_BEntities db = new EComm_Sum25_BEntities();
+        static Mapper  GetMapper() {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Product, ProductDTO>().ReverseMap();
+            });
+            return new Mapper(config);
+        }
+        // GET: Order
+        public ActionResult Index()
+        {
+            var data = db.Products.ToList();
+            var products = GetMapper().Map<List<ProductDTO>>(data);
+
+            return View(products);
+        }
+        public ActionResult AddtoCart(int id) {
+            var p = db.Products.Find(id);
+            var pdto = GetMapper().Map<ProductDTO>(p);
+            pdto.Qty = 1;
+            List<ProductDTO> cart = null;
+            if (Session["cart"] == null)
+            {
+                cart = new List<ProductDTO>();
+            }
+            else {
+                cart =(List<ProductDTO>) Session["cart"];
+            }
+
+            cart.Add(pdto);
+            Session["cart"] = cart;
+            
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Cart() {
+            var cart = (List<ProductDTO>)Session["cart"];
+            return View(cart);
+        }
+    }
+}
--- IntroEF/IntroEF/Controllers/StudentController.cs
+++ IntroEF/IntroEF/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using IntroEF.EF;
+﻿using AutoMapper;
+using IntroEF.DTOs;
+using IntroEF.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +23,20 @@ namespace IntroEF.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Create(Student s)
+        public ActionResult Create(StudentDTO s)
         {
-            db.Students.Add(s);
+            //var config = new MapperConfiguration(cfg => {
+            //    cfg.CreateMap<>();
+            //});
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<StudentDTO,Student>().ReverseMap();
+            });
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Student>(s);
+
+            db.Students.Add(data);
             db.SaveChanges();
             return RedirectToAction("Index");
 
@@ -32,6 +45,37 @@ namespace IntroEF.Controllers
             var data = db.Students.Find(id);
             return View(data);
 
+            
+        }
+        [HttpGet]
+        public ActionResult Edit(int id) { 
+            var student = db.Students.Find(id);
+            return View(student);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            var exobj = db.Students.Find(s.Id);
+            exobj.Name = s.Name;
+            exobj.Cgpa = s.Cgpa;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+        [HttpGet]
+        public ActionResult Delete(int id) {
+            var exObj = db.Students.Find(id);
+            return View(exObj);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, string choice) {
+            if (choice.Equals("Yes")) {
+                var exobj = db.Students.Find(id);
+                db.Students.Remove(exobj);
+                db.SaveChanges();
+                TempData["Msg"] = exobj.Name +" deleted";
+            }
+            return RedirectToAction("Index");
             
         }
     }
